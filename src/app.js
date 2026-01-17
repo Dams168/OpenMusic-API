346c63e16d1b74de33012ac30f2ae5c443b90e4a
Dams168 <adamrohman2003@gmail.com>
2026-01-17 19:16:21 +0700

    feat: setup database

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
-import express from 'express';
+import app from './server/index.js';
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://${host}:${PORT}`);
 });
