5c2c68466414532b369b58e67697640b772453d6
Dams168 <adamrohman2003@gmail.com>
2026-01-18 19:12:16 +0700

    feat: songs CRUD API

---
+++ src/repositories/song-repositories.js
@@ -0,0 +1,54 @@
+import { Pool } from 'pg';
+import { nanoid } from 'nanoid';
+
+class SongRepositories {
+  constructor() {
+    this.pool = new Pool();
+  }
+
+  async createSong({ title, year, genre, performer, duration, album_id }) {
+    const id = `song-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO songs (id, title, year, genre, performer, duration, album_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+      values: [id, title, year, genre, performer, duration, album_id],
+    };
+    const result = await this.pool.query(query);
+    return result.rows[0];
+  }
+
+  async getSongs() {
+    const result = await this.pool.query('SELECT id, title, performer FROM songs');
+    return result.rows;
+  }
+
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM songs WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this.pool.query(query);
+    return result.rows[0];
+  }
+
+  async updateSongById({ id, title, year, genre, performer, duration, album_id }) {
+    const query = {
+      text: 'UPDATE songs SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, album_id = $6 WHERE id = $7 RETURNING id',
+      values: [title, year, genre, performer, duration, album_id, id],
+    };
+    const result = await this.pool.query(query);
+    return result.rows[0];
+  }
+
+  async deleteSongById(id) {
+    const query = {
+      text: 'DELETE FROM songs WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+    const result = await this.pool.query(query);
+    return result.rows[0];
+  }
+}
+
+export default new SongRepositories();
---
+++ src/validator/song-validate.js
@@ -0,0 +1,19 @@
+import Joi from 'joi';
+
+export const songPayloadSchema = Joi.object({
+  title: Joi.string().required(),
+  year: Joi.number().integer().required(),
+  performer: Joi.string().required(),
+  genre: Joi.string().required(),
+  duration: Joi.number().integer().optional(),
+  album_id: Joi.string().optional(),
+});
+
+export const updateSongPayloadSchema = Joi.object({
+  title: Joi.string().required(),
+  year: Joi.number().integer().required(),
+  performer: Joi.string().required(),
+  genre: Joi.string().required(),
+  duration: Joi.number().integer().optional(),
+  album_id: Joi.string().optional(),
+});
